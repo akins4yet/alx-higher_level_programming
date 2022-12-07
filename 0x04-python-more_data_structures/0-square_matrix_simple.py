b2add3622279c3a2da595d8db9f4e56a62fb69dd
akins4yet <akins42good@.com>
2022-12-07 09:17:06 -0800

    init commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix != None:
+        new = []
+        for rows in matrix:
+            new.append(list(map(lambda x: x**2, rows)))
+        return (new)
+    return None
