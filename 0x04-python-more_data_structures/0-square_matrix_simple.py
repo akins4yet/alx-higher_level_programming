b0416a7490ac302b343e0a432cac0f2cad426b8d
Tijani Akinyemi <akins42good@gmail.com>
2023-10-11 12:09:12 -0700

    init commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,4 @@ def square_matrix_simple(matrix=[]):
         news = []
         for rows in matrix:
             news.append(list(map(lambda x: x**2, rows)))
-sh: 1: q: not found
-    return None
+    return news
